7fd997b90088113365f19aad3ad6f0c7579c90c3
Simon Phillips <simonxphillips@gmail.com>
2019-07-21 13:11:46 -0400

    Initial commit

---
+++ Mememoji/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Mememoji
+//
+//  Created by Simon Phillips on 7/21/19.
+//  Copyright © 2019 Simon Phillips. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Mememoji/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Mememoji
+//
+//  Created by Simon Phillips on 7/21/19.
+//  Copyright © 2019 Simon Phillips. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
